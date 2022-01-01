7c8297641ef5b1098804dff514b488a25b7f2c6e
ALeX <alexdeoliveira.poli@gmail.com>
2022-01-01 11:22:15 -0300

    Alignment and alignment guides – Layout and Geometry SwiftUI Tutorial 2/6 - Customized leading edge alignmentGuide

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -9,15 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .lastTextBaseline) {
-            Text("live")
-                .font(.caption)
-            Text("long")
-            Text("and")
-                .font(.title)
-            Text("prosper")
-                .font(.largeTitle)
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in
+                        CGFloat(position) * -10
+                    }
+            }
         }
+        .background(Color.red)
+        .frame(width: 300, height: 300)
+        .background(Color.blue)
     }
 }
 
