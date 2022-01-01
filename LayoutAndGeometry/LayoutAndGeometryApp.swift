cdc6a75ae5e03a5bc462c8731c9aeb4169b3e415
ALeX <alexdeoliveira.poli@gmail.com>
2022-01-01 09:53:01 -0300

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Alex Oliveira on 01/01/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
